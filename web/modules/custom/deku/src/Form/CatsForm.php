62580fe73499689c733cba13d992b1c0a58978af
Deku <gurycvadym510@gmail.com>
2021-08-28 13:06:35 +0300

    Add ajax form validation

--- web/modules/custom/deku/src/Form/CatsForm.php
+++ web/modules/custom/deku/src/Form/CatsForm.php
@@ -28,12 +28,22 @@ class CatsForm extends ConfigFormBase{
       '#title' => $this
         ->t("Your cat's name:"),
       '#description' => $this->t("Min 2 and max 32 characters"),
-      '#default_value'=> $config->get("deku.source_text")
+      '#default_value'=> $config->get("deku.source_text"),
+      '#attributes' => array('min-length' => '2'),
     );
-$form['email'] = array(
-  '#type' => 'email',
-  '#title' => $this->t('Your email:'),
-);
+    $form['email'] = array(
+      '#type' => 'email',
+      '#title' => $this->t('Your email:'),
+      '#default_value'=> $config->get("deku.email"),
+      '#required' => TRUE,
+      '#ajax' => [
+        'callback'=>'::validateEmailAjax',
+        'event' => 'input',
+      ],
+      '#decription' => $this->t("Email is allowed for example: example@mail.com
+      "),
+      
+    );
 
     $form['add_cat'] = array(
       '#type' => 'submit',
@@ -45,6 +55,16 @@ $form['email'] = array(
     );
     return $form;
   }
+  public function validateEmailAjax(array &$form, FormStateInterface $form_state){
+    $response = new AjaxResponse();
+    if ($this->stateForm == "noerror") {
+      $response->addCommand(new HtmlCommand('.highlighted .section', ""));
+    }else {
+      $response->addCommand(new HtmlCommand('.highlighted .section',
+      '<div data-drupal-messages-fallback="" class="hidden"></div><div class="alert-wrapper" data-drupal-messages><div role aria-label="'.$this->stateForm[0].' message" class="alert alert-dismissible fade show col-12 alert-'.$this->stateForm[0].'" role="'.$this->stateForm[2].'"><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>' . $this->stateForm[1] . '</div></div>'));
+    }
+    return $response;
+  }
   /**
    * {@inheritdoc}
    */
@@ -52,20 +72,27 @@ $form['email'] = array(
 
     $form_len = strlen($form_state->getValue('cats_name'));
 
-    if ($form_len < 3) {
-
+    if (!filter_var($form_state->getValue('email'), FILTER_VALIDATE_EMAIL)) {
+      $message = 'The email is not valid. Please enter a valid email';
+      $form_state->setErrorByName('email', $this->t($message));
+      $this->stateForm = [
+        'danger', $message, "alert", "email"
+      ];
+    }elseif ($form_len < 3) {
       $message = 'The cat name is too short. Please enter a valid cat name';
       $form_state->setErrorByName('cats_name', $this->t($message));
       $this->stateForm = [
-        'danger', $message, "alert"
+        'danger', $message, "alert", "cats_name"
       ];
-
     }elseif ($form_len > 32) {
-
       $message = 'The cat name is too long. Please enter a valid cat name';
       $form_state->setErrorByName('cats_name', $this->t($message));
       $this->stateForm = [
-        'danger', $message, "alert"
+        'danger', $message, "alert", "cats_name"
+      ];
+    }else {
+      $this->stateForm [
+        "noerror"
       ];
     }
   }
@@ -73,9 +100,10 @@ $form['email'] = array(
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    
+
     $config = $this->config('deku.settings');
     $config->set('deku.source_text', $form_state->getValue('cats_name'));
+    $config->set('deku.email', $form_state->getValue('email'));
     $config->save();
     $message = 'The cat name has been saved';
     $this
@@ -89,11 +117,15 @@ $form['email'] = array(
   public function setMessage(array $form, FormStateInterface $form_state) {
 
     $response = new AjaxResponse();
-    $response->addCommand(
+    if ($this->stateForm[0] == 'noerror') {
+      $response->addCommand(new HtmlCommand('.highlighted .section', ""));
+    }else {
+      $response->addCommand(
       new HtmlCommand(
         '.highlighted .section',
         '<div data-drupal-messages-fallback="" class="hidden"></div><div class="alert-wrapper" data-drupal-messages><div role aria-label="'.$this->stateForm[0].' message" class="alert alert-dismissible fade show col-12 alert-'.$this->stateForm[0].'" role="'.$this->stateForm[2].'"><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>' . $this->stateForm[1] . '</div></div>'),
-    );
+      );
+    }
     return $response;
 
    }
