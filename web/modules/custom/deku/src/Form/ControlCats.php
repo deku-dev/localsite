91b80a550fe2046c734a26c7ecebcef06d7b05e0
Deku <gurycvadym510@gmail.com>
2021-09-21 09:51:04 +0300

    Create form edit or delete entry

---
+++ web/modules/custom/deku/src/Form/ControlCats.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\deku\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\HtmlCommand;
+
+class ControlCats extends ConfigFormBase{
+  
+}
